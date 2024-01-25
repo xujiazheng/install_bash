9bf2625963300554dfae1bedfd4374e56277526e
xujiazheng <xujiazheng@youzan.com>
2024-01-25 20:58:04 +0800

    chore: 优化node脚本

--- env/node.sh
+++ env/node.sh
@@ -2,13 +2,13 @@
 
 # 安装nodejs
 cd /etc/yum.repos.d
-curl --silent --location https://rpm.nodesource.com/setup_14.x | bash -
+curl --silent --location https://rpm.nodesource.com/setup_18.x | bash -
 yum update -y
 yum install -y nodejs && echo "nodejs install ok"
 
 npm config set registry https://registry.npmmirror.com
 
-npm install -g pm2,nrm,nvm
+npm install -g pm2 nrm nvm
 echo "global npm installed"
 
 
